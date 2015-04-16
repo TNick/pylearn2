9dfb4ce652b779faf3266226ce7a4a6b111b5de0
Nicu Tofan <nicu.tofan@gmail.com>
2015-04-16 13:47:12 +0300

    Some resources from the www

--- pylearn2/sandbox/misc/__init__.py
+++ pylearn2/sandbox/misc/__init__.py
@@ -1,4 +1,27 @@
 """
 Various code pices related to pylearn2 from the World Wild Web.
+
+Other links to check out:
+
+use_images_in_pylearn2
+    https://github.com/CORDEA/use_images_in_pylearn2
+    http://cordea.hatenadiary.com/entry/2015/02/22/165713
+
+pylearn2MLPO
+    https://github.com/ssamot/pylearn2MLPO
+    
+Pylearn2 configurations and examples
+    https://github.com/AdamStone/pylearn-code
+    
+Various experiments with neural networks as IPython Notebooks utilizing Pylearn2/Theano
+    https://github.com/mkraemer67/nn_notebooks
+    
+med_ml with pylearn2
+    https://github.com/jackal092927/pylearn2_med
+    
+Select Machine Learning Papers implemented with pylearn2
+    https://github.com/KennethPierce/MLResearch
+    
+    
 """
 
